1aa655509aaf8c21dc62e70fad42dccf2ee98012
h-navi <hanafinaufal@protonmail.com>
2021-07-21 11:42:50 +0700

    rapikan form setting

--- controllers/base/SettingController.php
+++ controllers/base/SettingController.php
@@ -3,9 +3,10 @@
 // You should not change it manually as it will be overwritten on next build
 
 namespace app\controllers\base;
+
 use Yii;
 use app\models\Setting;
-    use app\models\search\SettingSearch;
+use app\models\search\SettingSearch;
 use yii\web\Controller;
 use yii\web\HttpException;
 use yii\helpers\Url;
@@ -15,209 +16,209 @@ use app\models\Action;
 use yii\web\UploadedFile;
 
 /**
-* SettingController implements the CRUD actions for Setting model.
-*/
+ * SettingController implements the CRUD actions for Setting model.
+ */
 class SettingController extends Controller
 {
 
 
-/**
-* @var boolean whether to enable CSRF validation for the actions in this controller.
-* CSRF validation is enabled only when both this property and [[Request::enableCsrfValidation]] are true.
-*/
-public $enableCsrfValidation = false;
-public function behaviors()
-{
-//NodeLogger::sendLog(Action::getAccess($this->id));
-//apply role_action table for privilege (doesn't apply to super admin)
-return Action::getAccess($this->id);
-}
+    /**
+     * @var boolean whether to enable CSRF validation for the actions in this controller.
+     * CSRF validation is enabled only when both this property and [[Request::enableCsrfValidation]] are true.
+     */
+    public $enableCsrfValidation = false;
+    public function behaviors()
+    {
+        //NodeLogger::sendLog(Action::getAccess($this->id));
+        //apply role_action table for privilege (doesn't apply to super admin)
+        return Action::getAccess($this->id);
+    }
 
-/**
-* Lists all Setting models.
-* @return mixed
-*/
-public function actionIndex()
-{
-$model = Setting::find()->all();
-$searchModel  = new SettingSearch;
-$dataProvider = $searchModel->search($_GET);
+    /**
+     * Lists all Setting models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $model = Setting::find()->all();
+        $searchModel  = new SettingSearch;
+        $dataProvider = $searchModel->search($_GET);
 
-Tabs::clearLocalStorage();
+        Tabs::clearLocalStorage();
 
-Url::remember();
-\Yii::$app->session['__crudReturnUrl'] = null;
+        Url::remember();
+        \Yii::$app->session['__crudReturnUrl'] = null;
 
-if($model == null){
-return $this->render('index', [
-'dataProvider' => $dataProvider,
-'searchModel' => $searchModel,
-]);
-}else{
-return $this->redirect(['view', 'id' => $model[0]["id"]]);
-}
-}
+        if ($model == null) {
+            return $this->render('index', [
+                'dataProvider' => $dataProvider,
+                'searchModel' => $searchModel,
+            ]);
+        } else {
+            return $this->redirect(['view', 'id' => $model[0]["id"]]);
+        }
+    }
 
-/**
-* Displays a single Setting model.
-* @param integer $id
-*
-* @return mixed
-*/
-public function actionView($id)
-{
-\Yii::$app->session['__crudReturnUrl'] = Url::previous();
-Url::remember();
-Tabs::rememberActiveState();
+    /**
+     * Displays a single Setting model.
+     * @param integer $id
+     *
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        \Yii::$app->session['__crudReturnUrl'] = Url::previous();
+        Url::remember();
+        Tabs::rememberActiveState();
 
-return $this->render('view', [
-'model' => $this->findModel($id),
-]);
-}
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
 
-/**
-* Creates a new Setting model.
-* If creation is successful, the browser will be redirected to the 'view' page.
-* @return mixed
-*/
-public function actionCreate()
-{
-$model = new Setting;
+    /**
+     * Creates a new Setting model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Setting;
 
-try {
-if ($model->load($_POST)) {
-    $logos = UploadedFile::getInstance($model, 'logo');
-        if($logos !=NULL){
+        try {
+            if ($model->load($_POST)) {
+                $logos = UploadedFile::getInstance($model, 'logo');
+                if ($logos != NULL) {
                     # store the source logos name
                     $model->logo = $logos->name;
                     $arr = explode(".", $logos->name);
                     $extension = end($arr);
-    
+
                     # generate a unique logos name
                     $model->logo = Yii::$app->security->generateRandomString() . ".{$extension}";
-    
+
                     # the path to save logos
                     // unlink(Yii::getAlias("@app/web/uploads/pengajuan/") . $oldFile);
-                    if(file_exists(Yii::getAlias("@app/web/uploads/setting/")) == false){
+                    if (file_exists(Yii::getAlias("@app/web/uploads/setting/")) == false) {
                         mkdir(Yii::getAlias("@app/web/uploads/setting/"), 0777, true);
                     }
                     $path = Yii::getAlias("@app/web/uploads/setting/") . $model->logo;
-        $logos->saveAs($path);
+                    $logos->saveAs($path);
                 }
-                
-    $bg_logins = UploadedFile::getInstance($model, 'bg_login');
-    if($bg_logins !=NULL){
-                # store the source bg_logins name
-                $model->bg_login = $bg_logins->name;
-                $arr = explode(".", $bg_logins->name);
-                $extension = end($arr);
 
-                # generate a unique bg_logins name
-                $model->bg_login = Yii::$app->security->generateRandomString() . ".{$extension}";
+                $bg_logins = UploadedFile::getInstance($model, 'bg_login');
+                if ($bg_logins != NULL) {
+                    # store the source bg_logins name
+                    $model->bg_login = $bg_logins->name;
+                    $arr = explode(".", $bg_logins->name);
+                    $extension = end($arr);
 
-                # the path to save bg_logins
-                // unlink(Yii::getAlias("@app/web/uploads/pengajuan/") . $oldFile);
-                if(file_exists(Yii::getAlias("@app/web/uploads/setting/")) == false){
-                    mkdir(Yii::getAlias("@app/web/uploads/setting/"), 0777, true);
+                    # generate a unique bg_logins name
+                    $model->bg_login = Yii::$app->security->generateRandomString() . ".{$extension}";
+
+                    # the path to save bg_logins
+                    // unlink(Yii::getAlias("@app/web/uploads/pengajuan/") . $oldFile);
+                    if (file_exists(Yii::getAlias("@app/web/uploads/setting/")) == false) {
+                        mkdir(Yii::getAlias("@app/web/uploads/setting/"), 0777, true);
+                    }
+                    $path = Yii::getAlias("@app/web/uploads/setting/") . $model->bg_login;
+                    $bg_logins->saveAs($path);
                 }
-                $path = Yii::getAlias("@app/web/uploads/setting/") . $model->bg_login;
-    $bg_logins->saveAs($path);
+
+                $bg_pins = UploadedFile::getInstance($model, 'bg_pin');
+                if ($bg_pins != NULL) {
+                    # store the source bg_pins name
+                    $model->bg_pin = $bg_pins->name;
+                    $arr = explode(".", $bg_pins->name);
+                    $extension = end($arr);
+
+                    # generate a unique bg_pins name
+                    $model->bg_pin = Yii::$app->security->generateRandomString() . ".{$extension}";
+
+                    # the path to save bg_pins
+                    // unlink(Yii::getAlias("@app/web/uploads/pengajuan/") . $oldFile);
+                    if (file_exists(Yii::getAlias("@app/web/uploads/setting/")) == false) {
+                        mkdir(Yii::getAlias("@app/web/uploads/setting/"), 0777, true);
+                    }
+                    $path = Yii::getAlias("@app/web/uploads/setting/") . $model->bg_pin;
+                    $bg_pins->saveAs($path);
+                }
+                if ($model->save()) {
+                    return $this->redirect(['view', 'id' => $model->id]);
+                }
+            } elseif (!\Yii::$app->request->isPost) {
+                $model->load($_GET);
             }
-            
-    $bg_pins = UploadedFile::getInstance($model, 'bg_pin');
-    if($bg_pins !=NULL){
-                # store the source bg_pins name
-                $model->bg_pin = $bg_pins->name;
-                $arr = explode(".", $bg_pins->name);
-                $extension = end($arr);
-
-                # generate a unique bg_pins name
-                $model->bg_pin = Yii::$app->security->generateRandomString() . ".{$extension}";
-
-                # the path to save bg_pins
-                // unlink(Yii::getAlias("@app/web/uploads/pengajuan/") . $oldFile);
-                if(file_exists(Yii::getAlias("@app/web/uploads/setting/")) == false){
-                    mkdir(Yii::getAlias("@app/web/uploads/setting/"), 0777, true);
-                }
-                $path = Yii::getAlias("@app/web/uploads/setting/") . $model->bg_pin;
-    $bg_pins->saveAs($path);
-            }
-    if($model->save()){
-    return $this->redirect(['view', 'id' => $model->id]);
+        } catch (\Exception $e) {
+            $msg = (isset($e->errorInfo[2])) ? $e->errorInfo[2] : $e->getMessage();
+            $model->addError('_exception', $msg);
+        }
+        return $this->render('create', ['model' => $model]);
     }
-} elseif (!\Yii::$app->request->isPost) {
-$model->load($_GET);
-}
-} catch (\Exception $e) {
-$msg = (isset($e->errorInfo[2]))?$e->errorInfo[2]:$e->getMessage();
-$model->addError('_exception', $msg);
-}
-return $this->render('create', ['model' => $model]);
-}
 
-/**
-* Updates an existing Setting model.
-* If update is successful, the browser will be redirected to the 'view' page.
-* @param integer $id
-* @return mixed
-*/
-public function actionUpdate($id)
-{
-$model = $this->findModel($id);
+    /**
+     * Updates an existing Setting model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
 
-if ($model->load($_POST) && $model->save()) {
-return $this->redirect(Url::previous());
-} else {
-return $this->render('update', [
-'model' => $model,
-]);
-}
-}
+        if ($model->load($_POST) && $model->save()) {
+            return $this->redirect(Url::previous());
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
 
-/**
-* Deletes an existing Setting model.
-* If deletion is successful, the browser will be redirected to the 'index' page.
-* @param integer $id
-* @return mixed
-*/
-public function actionDelete($id)
-{
-try {
-$this->findModel($id)->delete();
-} catch (\Exception $e) {
-$msg = (isset($e->errorInfo[2]))?$e->errorInfo[2]:$e->getMessage();
-\Yii::$app->getSession()->addFlash('error', $msg);
-return $this->redirect(Url::previous());
-}
+    /**
+     * Deletes an existing Setting model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        try {
+            $this->findModel($id)->delete();
+        } catch (\Exception $e) {
+            $msg = (isset($e->errorInfo[2])) ? $e->errorInfo[2] : $e->getMessage();
+            \Yii::$app->getSession()->addFlash('error', $msg);
+            return $this->redirect(Url::previous());
+        }
 
-// TODO: improve detection
-$isPivot = strstr('$id',',');
-if ($isPivot == true) {
-return $this->redirect(Url::previous());
-} elseif (isset(\Yii::$app->session['__crudReturnUrl']) && \Yii::$app->session['__crudReturnUrl'] != '/') {
-Url::remember(null);
-$url = \Yii::$app->session['__crudReturnUrl'];
-\Yii::$app->session['__crudReturnUrl'] = null;
+        // TODO: improve detection
+        $isPivot = strstr('$id', ',');
+        if ($isPivot == true) {
+            return $this->redirect(Url::previous());
+        } elseif (isset(\Yii::$app->session['__crudReturnUrl']) && \Yii::$app->session['__crudReturnUrl'] != '/') {
+            Url::remember(null);
+            $url = \Yii::$app->session['__crudReturnUrl'];
+            \Yii::$app->session['__crudReturnUrl'] = null;
 
-return $this->redirect($url);
-} else {
-return $this->redirect(['index']);
-}
-}
+            return $this->redirect($url);
+        } else {
+            return $this->redirect(['index']);
+        }
+    }
 
-/**
-* Finds the Setting model based on its primary key value.
-* If the model is not found, a 404 HTTP exception will be thrown.
-* @param integer $id
-* @return Setting the loaded model
-* @throws HttpException if the model cannot be found
-*/
-protected function findModel($id)
-{
-if (($model = Setting::findOne($id)) !== null) {
-return $model;
-} else {
-throw new HttpException(404, 'The requested page does not exist.');
-}
-}
+    /**
+     * Finds the Setting model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Setting the loaded model
+     * @throws HttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Setting::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new HttpException(404, 'The requested page does not exist.');
+        }
+    }
 }
--- views/setting/_form.php
+++ views/setting/_form.php
@@ -4,84 +4,201 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use \dmstr\bootstrap\Tabs;
 use kartik\file\FileInput;
+
 /**
-* @var yii\web\View $this
-* @var app\models\Setting $model
-* @var yii\widgets\ActiveForm $form
-*/
+ * @var yii\web\View $this
+ * @var app\models\Setting $model
+ * @var yii\widgets\ActiveForm $form
+ */
 
 ?>
 
 <div class="box box-info">
     <div class="box-body">
-        <?php $form = ActiveForm::begin([
-        'id' => 'Setting',
-        'layout' => 'horizontal',
-        'enableClientValidation' => true,
-        'errorSummaryCssClass' => 'error-summary alert alert-error'
-        ]
+        <?php $form = ActiveForm::begin(
+            [
+                'id' => 'Setting',
+                'layout' => 'horizontal',
+                'enableClientValidation' => true,
+                'errorSummaryCssClass' => 'error-summary alert alert-error'
+            ]
         );
         ?>
-			<?= $form->field($model, 'pin')->textInput(['type'=>'number']) ?>
-            
-			<?= $form->field($model, 'link_download_apk')->textInput(['maxlength' => true]) ?>
-			<?= $form->field($model, 'nama_web')->textInput(['maxlength' => true]) ?>
-			<?= $form->field($model, 'alamat')->textarea(['rows' => 6]) ?>
-			<?= $form->field($model, 'slogan_web')->textarea(['rows' => 6]) ?>   
-            <?= $form->field($model, 'logo',[
-                    
-                    'options' => ['tag' => false]])->widget(FileInput::classname(), [
-                'options' => ['accept' => 'file/*'],
-                'pluginOptions' => [
-                    'allowedFileExtensions' => ['png','jpg','jpeg'],
-                    'maxFileSize' => 6500,
-                    'showRemove' => false,
-                    'showUpload' => false,
-                    'showCaption' => false,
-                    'dropZoneEnabled' => false,
-                    'browseLabel' => 'Upload File',
-                ],
-                ]);?>
-                <br>
-                <hr>
-            <?= $form->field($model, 'bg_login',[
-                    
-                    'options' => ['tag' => false]])->widget(FileInput::classname(), [
-                'options' => ['accept' => 'file/*'],
-                'pluginOptions' => [
-                    'allowedFileExtensions' => ['png','jpg','jpeg'],
-                    'maxFileSize' => 6500,
-                    'showRemove' => false,
-                    'showUpload' => false,
-                    'showCaption' => false,
-                    'dropZoneEnabled' => false,
-                    'browseLabel' => 'Upload File',
-                ],
-                ]);?>
-                
-                <br>
-                <hr>
-            <?= $form->field($model, 'bg_pin',[
-                    
-                    'options' => ['tag' => false]])->widget(FileInput::classname(), [
-                'options' => ['accept' => 'file/*'],
-                'pluginOptions' => [
-                    'allowedFileExtensions' => ['png','jpg','jpeg'],
-                    'maxFileSize' => 6500,
-                    'showRemove' => false,
-                    'showUpload' => false,
-                    'showCaption' => false,
-                    'dropZoneEnabled' => false,
-                    'browseLabel' => 'Upload File',
-                ],
-                ]);?>
-                <br>
-                     <hr/>
-        <?php echo $form->errorSummary($model); ?>
         <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <?= $form->field($model, 'nama_web', [
+                    'template' => '
+                        {label}
+                        {input}
+                        {error}
+                    ',
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ],
+                    'labelOptions' => [
+                        'class' => 'control-label'
+                    ],
+                    'options' => ['tag' => false]
+                ])->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <?= $form->field($model, 'link_download_apk', [
+                    'template' => '
+                        {label}
+                        {input}
+                        {error}
+                    ',
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ],
+                    'labelOptions' => [
+                        'class' => 'control-label'
+                    ],
+                    'options' => ['tag' => false]
+                ])->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <?= $form->field($model, 'pin', [
+                    'template' => '
+                        {label}
+                        {input}
+                        {error}
+                    ',
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ],
+                    'labelOptions' => [
+                        'class' => 'control-label'
+                    ],
+                    'options' => ['tag' => false]
+                ])->textInput(['type' => 'number']) ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-6">
+                <?= $form->field($model, 'alamat', [
+                    'template' => '
+                        {label}
+                        {input}
+                        {error}
+                    ',
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ],
+                    'labelOptions' => [
+                        'class' => 'control-label'
+                    ],
+                    'options' => ['tag' => false]
+                ])->textarea(['rows' => 6]) ?>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-6">
+                <?= $form->field($model, 'slogan_web', [
+                    'template' => '
+                        {label}
+                        {input}
+                        {error}
+                    ',
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ],
+                    'labelOptions' => [
+                        'class' => 'control-label'
+                    ],
+                    'options' => ['tag' => false]
+                ])->textarea(['rows' => 6]) ?>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <?= $form->field($model, 'logo', [
+                    'template' => '
+                                {label}
+                                {input}
+                                {error}
+                            ',
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ],
+                    'labelOptions' => [
+                        'class' => 'control-label'
+                    ],
+                    'options' => ['tag' => false]
+                ])->widget(FileInput::classname(), [
+                    'options' => ['accept' => 'file/*'],
+                    'pluginOptions' => [
+                        'allowedFileExtensions' => ['png', 'jpg', 'jpeg', 'pdf', 'doc', 'docx'],
+                        'maxFileSize' => 6500,
+                        'dropZoneEnabled' => false,
+                        'showCaption' => true,
+                        'showRemove' => false,
+                        'showUpload' => false,
+                        'browseLabel' => 'Upload File',
+                    ],
+                ]); ?>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <?= $form->field($model, 'bg_login', [
+                    'template' => '
+                                {label}
+                                {input}
+                                {error}
+                            ',
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ],
+                    'labelOptions' => [
+                        'class' => 'control-label'
+                    ],
+                    'options' => ['tag' => false]
+                ])->widget(FileInput::classname(), [
+                    'options' => ['accept' => 'file/*'],
+                    'pluginOptions' => [
+                        'allowedFileExtensions' => ['png', 'jpg', 'jpeg', 'pdf', 'doc', 'docx'],
+                        'maxFileSize' => 6500,
+                        'dropZoneEnabled' => false,
+                        'showCaption' => true,
+                        'showRemove' => false,
+                        'showUpload' => false,
+                        'browseLabel' => 'Upload File',
+                    ],
+                ]); ?>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                <?= $form->field($model, 'bg_pin', [
+                    'template' => '
+                                {label}
+                                {input}
+                                {error}
+                            ',
+                    'inputOptions' => [
+                        'class' => 'form-control'
+                    ],
+                    'labelOptions' => [
+                        'class' => 'control-label'
+                    ],
+                    'options' => ['tag' => false]
+                ])->widget(FileInput::classname(), [
+                    'options' => ['accept' => 'file/*'],
+                    'pluginOptions' => [
+                        'allowedFileExtensions' => ['png', 'jpg', 'jpeg', 'pdf', 'doc', 'docx'],
+                        'maxFileSize' => 6500,
+                        'dropZoneEnabled' => false,
+                        'showCaption' => true,
+                        'showRemove' => false,
+                        'showUpload' => false,
+                        'browseLabel' => 'Upload File',
+                    ],
+                ]); ?>
+            </div>
+
+        </div>
+
+        <?php echo $form->errorSummary($model); ?>
+        <div class="row" style="padding-top: 2rem;">
             <div class="col-md-offset-3 col-md-10">
-                <?=  Html::submitButton('<i class="fa fa-save"></i> Simpan', ['class' => 'btn btn-success']); ?>
-                <?=  Html::a('<i class="fa fa-chevron-left"></i> Kembali', ['index'], ['class' => 'btn btn-default']) ?>
+                <?= Html::submitButton('<i class="fa fa-save"></i> Simpan', ['class' => 'btn btn-success']); ?>
+                <?= Html::a('<i class="fa fa-chevron-left"></i> Kembali', ['index'], ['class' => 'btn btn-default']) ?>
             </div>
         </div>
 
