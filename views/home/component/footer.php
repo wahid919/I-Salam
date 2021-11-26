46adfeb891baa335f57b616d85f3c8e8c767d14d
H-navi <hanafinaufal@protonmail.com>
2021-11-26 15:24:43 +0700

    edit homepage

--- views/home/component/footer.php
+++ views/home/component/footer.php
@@ -30,10 +30,10 @@ $icons = \Yii::$app->request->baseUrl . "/uploads/setting/" . $setting->logo;
               <center>
 
                 <div class="social__icons">
-                  <a href="<?= $setting->facebook ?>" target="_blank"><i class="fa fa-facebook"></i></a>
-                  <a href="<?= $setting->twitter ?>" target="_blank"><i class="fa fa-twitter"></i></a>
-                  <a href="<?= $setting->instagram ?>" target="_blank"><i class="fa fa-instagram"></i></a>
-                  <a href="https://t.me/isalamkarim" target="_blank"><i class="fa fa-telegram"></i></a>
+                  <a href="<?= $setting->facebook ?>" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                  <a href="<?= $setting->twitter ?>" target="_blank"><i class="fab fa-twitter-square"></i></a>
+                  <a href="<?= $setting->instagram ?>" target="_blank"><i class="fab fa-instagram-square"></i></a>
+                  <a href="https://t.me/isalamkarim" target="_blank"><i class="fab fa-telegram-plane"></i></a>
                 </div><!-- /.col-xl-2 -->
               </center>
             </ul>
