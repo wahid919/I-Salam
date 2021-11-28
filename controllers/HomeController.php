a8bc6e99c57560577261f8a9642bde9cbc8b6740
Fachru Wildan <fachruwildan1@gmail.com>
2021-11-28 13:35:08 +0700

    update : tampilan halaman bayar

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -131,12 +131,57 @@ class HomeController extends Controller
 
     }
     public function actionBayar($id){
-        $this->layout = false;
+        // $this->layout = false;
+       
+        // $pendanaan = Pendanaan::findOne(['pendanaan_id'=>$id]);
+
+        // return $this->render('bayar', [
+        //     'pembayaran' => $pembayaran,
+        //     'pendanaan' => $pendanaan,
+            
+        // ]);
         $pembayaran = Pembayaran::findOne(['id'=>$id]);
+        $setting = Setting::find()->one();
+        $icon = \Yii::$app->request->baseUrl . "/uploads/setting/" . $setting->logo;
+        $bg_login = \Yii::$app->request->baseUrl . "/uploads/setting/" . $setting->bg_login;
+        $bg = \Yii::$app->request->baseUrl . "/uploads/setting/" . $setting->bg_pin;
+        $organisasis = Organisasi::find()->where(['flag' => 1])->all();
+        $lembagas = LembagaPenerima::find()->where(['flag' => 1])->all();
+        $count_program = Pendanaan::find()->count();
+        $count_wakif = User::find()->where(['role_id' => 5])->count();
+        $model = new HubungiKami;
+        $testimonials = Testimonials::find()->all();
+        $pendanaans = Pendanaan::find()->where(['status_id' => 2])->limit(6)->all();
+        
+        $news = Berita::find()->limit(6)->all();
+
+
+        if ($model->load($_POST)) {
+            $model->status = 0;
+
+            if ($model->save()) {
+                Yii::$app->session->setFlash('success', "Data berhasil disimpan.");
+            } else {
+                Yii::$app->session->setFlash('error', "Data tidak berhasil disimpan.");
+            }
+            return $this->redirect(['home/index']);
+        }
 
         return $this->render('bayar', [
             'pembayaran' => $pembayaran,
-            
+            'setting' => $setting,
+            // 'snapToken' => $snapToken,
+            'count_program' => $count_program,
+            'count_wakif' => $count_wakif,
+            'organisasis' => $organisasis,
+            'lembagas' => $lembagas,
+            'icon' => $icon,
+            'bg_login' => $bg_login,
+            'bg' => $bg,
+            'testimonials' => $testimonials,
+            'model' => $model,
+            'pendanaans' => $pendanaans,
+            'news' => $news,
         ]);
 
     }
