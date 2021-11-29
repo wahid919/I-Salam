b4bff0cbd1ed43f24dc21d61b4ffaf8daf064704
Fachru Wildan <fachruwildan1@gmail.com>
2021-11-29 15:50:45 +0700

    update : show result after pay

--- views/home/bayar.php
+++ views/home/bayar.php
@@ -357,23 +357,31 @@
 </script>
 <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-lrPe45BCGoT9yG2O"></script>
 
-    <!-- <pre><div id="result-json">JSON result will appear here after payment:<br></div></pre>  -->
+    <pre><div id="result-json">JSON result will appear here after payment:<br></div></pre> 
 <script type="text/javascript">
 
       function setGlobal() {
-        snap.pay('<?=$pembayaran->code?>', {
+        window.snap.pay('<?=$pembayaran->code?>', {
           // Optional
           onSuccess: function(result){
             /* You may add your own js here, this is just example */ document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
+            alert("payment success!"); console.log(result);
+            // alert("payment success!"); console.log(result);
           },
           // Optional
           onPending: function(result){
+            alert("payment pending!"); console.log(result);
             /* You may add your own js here, this is just example */ document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
           },
           // Optional
           onError: function(result){
             /* You may add your own js here, this is just example */ document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
-          }
+            alert("payment error!"); console.log(result);
+          },
+          onClose: function(){
+    /* You may add your own implementation here */
+    alert('you closed the popup without finishing the payment');
+  }
         });
       };
       setGlobal();
