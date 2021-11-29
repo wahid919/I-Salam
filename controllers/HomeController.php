8b98ab788080779251bb43a5fd6efdea749de643
Fachru Wildan <fachruwildan1@gmail.com>
2021-11-29 17:50:54 +0700

    update : menampilkan data pembayaran dan update status pembayaran

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -750,7 +750,7 @@ class HomeController extends Controller
     {
         $setting = Setting::find()->one();
         $icon = \Yii::$app->request->baseUrl . "/uploads/setting/" . $setting->logo;
-        // $pembayarans = Pembayaran::find()->where(['user_id' => Yii::$app->user->identity->id])->all();
+        $data_all = Pembayaran::find()->where(['user_id' => Yii::$app->user->identity->id])->all();
 
         $query = Pembayaran::find()->where(['user_id' => Yii::$app->user->identity->id]);
         $count = $query->count();
@@ -759,6 +759,35 @@ class HomeController extends Controller
             ->limit($pagination->limit)
             ->orderBy(['created_at' => SORT_DESC])
             ->all();
+            foreach($data_all as $data){
+                $wf = Pembayaran::find()->where(['id' => $data->id])->one();
+                $a = $this->findMidtrans($wf->kode_transaksi);
+
+                if($a->status_code == "404"){
+                     $wf->status_id = 5;
+                }else{
+                    if($a->transaction_status == "pending"){
+                         $wf->status_id = 5;
+                    }elseif($a->transaction_status == "capture" || $a->transaction_status == "settlement" ){
+                         $wf->status_id = 6;
+                         $wf->tanggal_konfirmasi = date('Y-m-d H:i:s');
+                    }elseif($a->transaction_status == "deny" || $a->transaction_status == "cancel" || $a->transaction_status == "expire" ){
+                         $wf->status_id = 8;
+                         $wf->tanggal_konfirmasi = date('Y-m-d H:i:s');
+                    }
+                }
+
+                if($a->status_code == "404"){
+                     $wf->jenis_pembayaran_id ="Tidak Ditemukan";
+                }else{
+                    if($a->payment_type == "cstore"){
+                         $wf->jenis_pembayaran_id = $a->store;
+                    }else{
+                         $wf->jenis_pembayaran_id = $a->payment_type;
+                    }
+                }
+                $wf->save();
+            }
 
         return $this->render('profile', [
             'setting' => $setting,
@@ -938,5 +967,32 @@ class HomeController extends Controller
         } else {
             throw new \yii\web\NotFoundHttpException("{$path} is not found!");
         }
+    }
+    protected function findMidtrans($id)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.sandbox.midtrans.com/v2/" . $id . "/status",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_POSTFIELDS => "\n\n",
+            CURLOPT_HTTPHEADER => array(
+                "Accept: application/json",
+                "Content-Type: application/json",
+                "Authorization: Basic U0ItTWlkLXNlcnZlci1MV1RfNVJHdkhsUk9sSWJtYUU4SzBudGI6"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $a = json_decode($response);
+        return $a;
     }
 }
--- views/home/profile.php
+++ views/home/profile.php
@@ -7,7 +7,8 @@
         <div class="col-lg-8 col-md-6 col-sm-12 col-12 profile-section">
             <h3 class="text-isalam-1 font-weight-bold text-detail-program">Transaksi</h3>
             <div class="row">
-                <?php foreach ($pembayarans as $pembayaran) { ?>
+                <?php foreach ($pembayarans as $pembayaran) {
+                    $code = $pembayaran->code; ?>
                     <div class="col-lg-6 col-md-6 col-sm-8 col-8 text-left border-bottom-3">
                         <p class="font-weight-bold"><?= $pembayaran->pendanaan->nama_pendanaan ?></p>
                         <p class="font-size-08"><?= \app\components\Tanggal::toReadableDate($pembayaran->created_at); ?> | <span class="text-isalam-1"><?= \app\components\Angka::toReadableHarga($pembayaran->nominal); ?></span></p>
@@ -29,8 +30,43 @@
                             $status = "btn-danger";
                         }
                         ?>
-                        <p><a href="#" class="btn btn-sm btn-program <?= $status ?>"><?= $pembayaran->status->name ?></a></p>
+                        <button id="pay-button-<?=$pembayaran->id ?>" class="btn btn-sm btn-program <?= $status ?>"> <?= $pembayaran->status->name ?></button>
+                        <!-- <p><a href="#" class="btn btn-sm btn-program <?= $status ?>"><?= $pembayaran->status->name ?></a></p> -->
                     </div>
+
+<script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-lrPe45BCGoT9yG2O"></script>
+<script type="text/javascript">
+    let kode_snap_<?=$pembayaran->id ?> = "<?= $code ?>";
+      document.getElementById('pay-button-<?=$pembayaran->id ?>').onclick = function(){
+          if(kode_snap_<?= $pembayaran->id ?> == null){
+            alert("Data Tidak Ditemukan");
+          }else{
+            snap.pay('<?=$pembayaran->code?>', {
+          // Optional
+          onSuccess: function(result){
+            /* You may add your own js here, this is just example */ 
+            // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
+          },
+          // Optional
+          onPending: function(result){
+            /* You may add your own js here, this is just example */ 
+            // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
+          },
+          // Optional
+          onError: function(result){
+            /* You may add your own js here, this is just example */ 
+            // document.getElementById('result-json').innerHTML += JSON.stringify(result, null, 2);
+          },
+      onClose: function() {
+        /* You may add your own implementation here */
+        // Swal.fire("Peringatan!", "Anda Belum Menyelesaikan Pembayaran", "error");
+      }
+        });
+          }
+        // SnapToken acquired from previous step
+        
+      };
+    </script>
                 <?php } ?>
             </div>
             <div class='d-flex justify-content-center pt-4'>
