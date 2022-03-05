b332fd77aa43c3a2ffca1654ee20e2042855f77d
Fachru Wildan <fachruwildan1@gmail.com>
2022-03-05 23:31:37 +0700

    update : api pembayaran(jenis(wakaf/infak))

--- controllers/api/PembayaranController.php
+++ controllers/api/PembayaranController.php
@@ -263,6 +263,7 @@ class PembayaranController extends \yii\rest\ActiveController
         $model->jenis_pembayaran_id = $val['jenis_pembayaran_id'] ?? '';
         $model->user_id = \Yii::$app->user->identity->id;
         $model->status_id = 5;
+        $model->jenis ="wakaf";
         // $model->tanggal_pembayaran = date('Y-m-d');
         $bukti_transaksis = UploadedFile::getInstanceByName('bukti_transaksi');
 
