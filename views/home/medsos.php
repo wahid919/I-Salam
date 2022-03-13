e906fc229fd4bafb9f0caba0ff38c89c2d72c34b
Fachru Wildan <fachruwildan1@gmail.com>
2022-03-14 03:58:34 +0700

    update : medsos

--- views/home/medsos.php
+++ views/home/medsos.php
@@ -4,11 +4,38 @@
       <div class="row heading heading-2 mb-40">
         <div class="col-sm-12 col-md-12 col-lg-12">
           <div class="title-img" style="background-image: url(<?= \Yii::$app->request->baseUrl . "/uploads/mosque.jpg" ?>);">
-            <div class="text-white title_about">Map</div>
+            <div class="text-white title_about">Medsos</div>
             <div class="title-overlay-ku">
             </div>
           </div>
-          <div id="map-canvas2" style="height: 400px; width: 100%"></div>
+
+          <div class="row">
+                  
+                  <div class="col-12 pt-12">
+                    <div class="row">
+                      <!-- <div class="col-4"> -->
+                        <a href="<?= $setting->facebook ?>" target="_blank">
+                          <img src="https://img.icons8.com/color/70/000000/facebook-new.png" alt="facebook" style="cursor:pointer"/>
+                        </a>
+                        <!-- </div> -->
+                        <a href="<?= $setting->twitter ?>" target="_blank">
+                          <img src="https://img.icons8.com/color/70/000000/twitter--v1.png" alt="twitter" style="cursor:pointer"/>
+                      </a>
+                        <!-- <div class="col-4"> -->
+                          <a href="<?= $setting->instagram ?>" target="_blank">
+                            <img src="https://img.icons8.com/color/70/000000/instagram--v1.png" alt="instagram" style="cursor:pointer"/>
+                        </a>
+                          <!-- </div> -->
+                          <!-- <div class="col-4">   -->
+                        <a href="<?= $setting->telegram ?>" target="_blank">
+                          <img src="https://img.icons8.com/color/70/000000/telegram-app--v1.png" alt="telegram" style="cursor:pointer"/>
+                      </a>
+                      <!-- </div> -->
+                    </div>
+                    
+                  </div>
+                </div>
+          <!-- <div id="map-canvas2" style="height: 400px; width: 100%"></div> -->
         </div><!-- /.col-lg-5 -->
       </div><!-- /.row -->
     </div><!-- /.container -->
