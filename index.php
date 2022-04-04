18db134064fa699fe0f0db6814ff35cd9c2096e4
H-navi <hanafinaufal@protonmail.com>
2022-04-04 15:28:25 +0700

    undo remove /web

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
-error_reporting(E_ALL^E_NOTICE);
-
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
-require(__DIR__ . 'vendor/autoload.php');
-require(__DIR__ . 'vendor/yiisoft/yii2/Yii.php');
-
-$config = require(__DIR__ . 'config/web.php');
-
-(new yii\web\Application($config))->run();
+/**
+ * Created by PhpStorm.
+ * User: K0D1K
+ * Date: 08/12/2018
+ * Time: 18:12
+ */
+header("Location: web");
