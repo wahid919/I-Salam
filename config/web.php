97813f49a883726e2a73ee508090f4fe6f5b9060
Fachru Wildan <fachruwildan1@gmail.com>
2022-04-08 14:37:10 +0700

    update : mengembalikan /web

--- config/web.php
+++ config/web.php
@@ -1,9 +1,7 @@
 <?php
-use \yii\web\Request;
+
 require_once 'Custom.php';
 $params = require(__DIR__ . '/params.php');
-
-$baseUrl = str_replace('/web', '', (new Request)->getBaseUrl());
 
 $config = [
     'id' => 'basic',
@@ -21,7 +19,6 @@ $config = [
             'secretV3' => Yii::$app->params['recaptcha3.secretKey'],
         ],
         'request' => [
-            'baseUrl' => $baseUrl,
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'secret',
         ],
